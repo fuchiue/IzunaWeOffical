6055c46814b6c28c0ddba3d89b127b75d38c83c5
LAMJOLIE <jolielam317@gmail.com>
2023-06-12 11:21:01 +0900

    ホームページ写真の自動な変更JSを追加した、検索機能のPHPを追加した

---
+++ data.php
@@ -0,0 +1,28 @@
+<?php
+//require_once "./data.php";
+//上記コードをファイルの最初に入れればデータベース接続できる
+function dbc()
+{
+    $host = "localhost";
+    $dbname = "izanadb";
+    $user = "root";
+    $pass = "root";
+    $dns = "mysql:host=$host; dbname=$dbname; charset=utf8";
+
+    try {
+        $pdo = new PDO(
+            $dns,
+            $user,
+            $pass,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]
+        );
+        return $pdo;
+    } catch (PDOException $e) {
+        exit($e->getMessage());
+    }
+}
+dbc();
+?>
