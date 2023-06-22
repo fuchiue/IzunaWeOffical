25046099a8fd974fa95954775713c84ab55013c3
Lim-z <kelvinlim630@gmail.com>
2023-06-22 11:45:14 +0900

    ログイン時(ホストとユーザ)のエラーメッセージが出ないのを修正しました。 フッターのところも：1.もしログインしてたら、「ログアウト」が表示される                            2.もしログインしてなかったら、「ログイン/新規登録」が表示される

--- Navbar/footer.php
+++ Navbar/footer.php
@@ -1,10 +1,26 @@
+<?php
+$id=null;
+if (isset($_SESSION["id"])) {
+    $id = $_SESSION["id"];
+}
+?>
 <footer>
-        <div id="footerSub">
-            <a href="#">このサイトについて</a>
-            <a href="../login_page_User.php">ログイン/新規登録</a>
-            <a href="#">よくある質問</a>
-        </div>
+    <div id="footerSub">
+        <a href="#">このサイトについて</a>
+        <?php
 
-        <p>CopyRight by IzanaWeOffical</p>
+        if ($id == null) {
+            ?>
+            <a href="login_page_User.php">ログイン/新規登録</a>
+            <?php
+        } else {
+            ?>
+            <a href="logout.php">ログアウト</a>
+            <?php
+        }
+        ?>
+    </div>
+
+    <p>CopyRight by IzanaWeOffical</p>
 
 </footer>
