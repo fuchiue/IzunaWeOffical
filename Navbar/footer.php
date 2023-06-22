caa15ec7f9e155d168699bfb7bfb19e0503b462a
fuchiue <fuchiue2220235@gmail.com>
2023-06-22 13:08:02 +0900

    フッターのエラーを修正ホーム

--- Navbar/footer.php
+++ Navbar/footer.php
@@ -5,26 +5,14 @@ if (isset($_SESSION["id"])) {
 }
 ?>
 <footer>
-<<<<<<< HEAD
-        <div id="footerSub">
-            <a href="index.php">このサイトについて</a>
-            <a href="login_page_User.php">ログイン/新規登録</a>
-            <a href="#">よくある質問</a>
-        </div>
-=======
     <div id="footerSub">
         <a href="#">このサイトについて</a>
         <?php
->>>>>>> Lim
 
         if ($id == null) {
-            ?>
-            <a href="login_page_User.php">ログイン/新規登録</a>
-            <?php
+            echo '<a href="login_page_User.php">ログイン/新規登録</a>';
         } else {
-            ?>
-            <a href="logout.php">ログアウト</a>
-            <?php
+            echo '<a href="logout.php">ログアウト</a>';
         }
         ?>
     </div>
