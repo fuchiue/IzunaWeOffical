c5f3002ef2928f6af3d542abf8c79782aaaec855
Lim-z <kelvinlim630@gmail.com>
2023-07-03 11:56:47 +0900

    IDが10001なのをGETからIDが取れるようにした

--- userpage_ViewOnly.php
+++ userpage_ViewOnly.php
@@ -4,7 +4,11 @@ require_once __DIR__ . "/data.php";
 // ログインからIDを取ってくる
 // $userid = filter_input(INPUT_POST, "user");
 
-$userid = $_SESSION["id"];
+if(isset($_GET["id"])){
+    $userid = $_GET["id"];
+}else{
+    header("Location: index.php");
+}
 // IDが入ってたら、TakeUserData,TakePostData,TakeEventDataファンクションを実行してデータを取得する
 if ($userid != null) {
     $userdata = TakeUserData($userid);
@@ -45,7 +49,7 @@ if ($userid != null) {
     <section id="selfInfo_TopArea">
         <!-- 写真 -->
         <div id="selfIcon_pic">
-            <img src="./image/HT_Fox3 (1).PNG">
+            <img src="<?= $userdata['ICON'] ?>">
         </div>
 
         <!-- 分割線 -->
