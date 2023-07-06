816ed892a0cd4278b0ea731f993f00af192e0de2
2220276 <2220276@ecc.ac.jp>
2023-07-06 10:43:25 +0900

    ログイン後画面のレイアウト調整

--- hostpage_AfterLogin.php
+++ hostpage_AfterLogin.php
@@ -34,230 +34,197 @@ $photodatas = TakePostData($id); //投稿した写真を取得
             <h1>ホストマイページ</h1>
             <p>Group Profile</p>
         </div>
+    </div>
 
-        <!-- 団体アイコン＋紹介エリア -->
-        <section id="selfInfo_TopArea">
-            <!-- 写真 -->
-            <div id="selfIcon_pic">
-                <img src="<?= $hostdata['ICON'] ?>">
-            </div>
+    <!-- 団体アイコン＋紹介エリア -->
+    <section id="selfInfo_TopArea">
+        <!-- 写真 -->
+        <div id="selfIcon_pic">
+            <img src="<?= $hostdata['ICON'] ?>">
+        </div>
 
-            <!-- 分割線 -->
-            <div id="redDecoBox"></div>
+        <!-- 分割線 -->
+        <div id="redDecoBox"></div>
 
-            <!-- 団体情報 -->
-            <div id="selfInfo_Box">
+        <!-- 団体情報 -->
+        <div id="selfInfo_Box">
 
-                <!-- 団体名　-->
-                <h1><?= $hostdata['OWNER_NAME'] ?></h1>
+            <!-- 団体名　-->
+            <h1><?= $hostdata['OWNER_NAME'] ?></h1>
 
-                <!-- 団体紹介文　-->
-                <p id="selfInfo_title">団体紹介文</p>
-                <div class="word-break">
-                    <div class="normal">
-                        <p><?= $hostdata['NOTE'] ?></p>
-                    </div>
+            <!-- 団体紹介文　-->
+            <p id="selfInfo_title">団体紹介文</p>
+            <div class="word-break">
+                <div class="normal">
+                    <p><?= $hostdata['NOTE'] ?></p>
                 </div>
             </div>
+        </div>
 
-        </section>
+    </section>
 
-        <!-- 投稿写真＋参加者一覧エリア　-->
+    <!-- 投稿写真＋参加者一覧エリア　-->
 
-        <section id="selfInfo_MidArea">
+    <section id="selfInfo_MidArea">
 
-            <!-- 3ボタン　-->
-            <div class="hostView_Btn">
-                <button id="imgBtn">写真</button>
-                <button id="attendantBtn">参加者の皆様</button>
-                <a href="postPic_Host.php" id="hostpostBtn">投稿</a>
-            </div>
+        <!-- 3ボタン　-->
+        <div class="hostView_Btn">
+            <button id="imgBtn">写真</button>
+            <button id="attendantBtn">参加者の皆様</button>
+            <a href="postPic_Host.php" id="hostpostBtn">投稿</a>
+        </div>
 
-            <div id="switchPage">
-                <!-- 投稿した写真　-->
-                <div id="hostPostImg">
+        <div id="switchPage">
+            <!-- 投稿した写真　-->
+            <div id="hostPostImg">
 
-                    <div class="scroll-bg">
-                        <div class="scroll-div">
-                            <div class="scroll-object">
-                                <?php
-                                $cont = 0; //表示回数を管理
-                                //三回表示ごとにタグを閉じる
-                                foreach ($photodatas as $photo) { //参加ユーザの数だけ回る
-                                    if ($cont == 0) {
-                                        echo '<div class="imgRow">'; //初めのタグ
-                                        echo "\n";
-                                    }
-
-                                    //写真の表示
-                                    echo '<a class="imgBox"><img src="' . $photo['PHOTO'] . '"></a>';
+                <div class="scroll-bg">
+                    <div class="scroll-div">
+                        <div class="scroll-object">
+                            <?php
+                            $cont = 0; //表示回数を管理
+                            //三回表示ごとにタグを閉じる
+                            foreach ($photodatas as $photo) { //参加ユーザの数だけ回る
+                                if ($cont == 0) {
+                                    echo '<div class="imgRow">'; //初めのタグ
                                     echo "\n";
-
-                                    //三回写真を表示したら終わりのタグそれ以外ならカウントアップ
-                                    if ($cont == 2) {
-                                        echo '</div>'; //終わりのタグ
-                                        echo "\n";
-                                        $cont = 0;
-                                    } else {
-                                        $cont++;
-                                    }
                                 }
 
-                                //ユーザが一人もいなかったとき
-                                if (!$photodatas) {
-                                    echo '<div class="imgRow">'; //始まりのタグ
-                                    echo "\n";
+                                //写真の表示
+                                echo '<a class="imgBox"><img src="' . $photo['PHOTO'] . '"></a>';
+                                echo "\n";
+
+                                //三回写真を表示したら終わりのタグそれ以外ならカウントアップ
+                                if ($cont == 2) {
                                     echo '</div>'; //終わりのタグ
                                     echo "\n";
+                                    $cont = 0;
+                                } else {
+                                    $cont++;
                                 }
-                                //ループを抜けた後タグを閉じていないときに閉じる
-                                if ($cont != 0) {
-                                    echo '</div>'; //終わりのタグ
-                                    echo "\n";
-                                }
+                            }
 
-                                ?>
-                            </div>
-                        </div>
-                    </div>
+                            //ユーザが一人もいなかったとき
+                            if (!$photodatas) {
+                                echo '<div class="imgRow">'; //始まりのタグ
+                                echo "\n";
+                                echo '</div>'; //終わりのタグ
+                                echo "\n";
+                            }
+                            //ループを抜けた後タグを閉じていないときに閉じる
+                            if ($cont != 0) {
+                                echo '</div>'; //終わりのタグ
+                                echo "\n";
+                            }
 
-
-                </div>
-                <!-- 参加者一覧　-->
-                <div id="attendantImg">
-                    <div class="scroll-bg">
-                        <div class="scroll-div">
-                            <div class="scroll-object">
-                                <?php
-                                $cont = 0; //表示回数を管理
-                                //三回表示ごとにタグを閉じる
-                                foreach ($joinedUser as $user) { //参加ユーザの数だけ回る
-
-                                    if ($cont == 0) {
-                                        echo '<div class="imgRow">'; //初めのタグ
-                                        echo "\n";
-                                    }
-                                    //写真の表示　対応するユーザのIDを変数で送っている。
-                                    echo '<a href="./userpage_ViewOnly.php?id=' . $user['USER_ID'] . '" class="imgBox"><img src="' . $user['ICON'] . '"></a>';
-                                    echo "\n";
-
-                                    //三回写真を表示したら終わりのタグそれ以外ならカウントアップ
-                                    if ($cont == 2) {
-                                        echo '</div>'; //終わりのタグ
-                                        echo "\n";
-                                        $cont = 0;
-                                    } else {
-                                        $cont++;
-                                    }
-                                }
-
-                                //ユーザが一人もいなかったとき
-                                if (!$joinedUser) {
-                                    echo '<div class="imgRow">'; //始まりのタグ
-                                    echo "\n";
-                                    echo '</div>'; //終わりのタグ
-                                    echo "\n";
-                                }
-                                //ループを抜けた後タグを閉じていないときに閉じる
-                                if ($cont != 0) {
-                                    echo '</div>'; //終わりのタグ
-                                    echo "\n";
-                                }
-
-                                ?>
-
-                            </div>
+                            ?>
                         </div>
                     </div>
                 </div>
 
+
             </div>
+            <!-- 参加者一覧　-->
+            <div id="attendantImg">
+                <div class="scroll-bg">
+                    <div class="scroll-div">
+                        <div class="scroll-object">
+                            <?php
+                            $cont = 0; //表示回数を管理
+                            //三回表示ごとにタグを閉じる
+                            foreach ($joinedUser as $user) { //参加ユーザの数だけ回る
 
-        </section>
+                                if ($cont == 0) {
+                                    echo '<div class="imgRow">'; //初めのタグ
+                                    echo "\n";
+                                }
+                                //写真の表示　対応するユーザのIDを変数で送っている。
+                                echo '<a href="./userpage_ViewOnly.php?id=' . $user['USER_ID'] . '" class="imgBox"><img src="' . $user['ICON'] . '"></a>';
+                                echo "\n";
 
-        <!-- イベント一覧＋ 応募者一覧　エリア　-->
-        <section id="selfInfo_BottomArea">
+                                //三回写真を表示したら終わりのタグそれ以外ならカウントアップ
+                                if ($cont == 2) {
+                                    echo '</div>'; //終わりのタグ
+                                    echo "\n";
+                                    $cont = 0;
+                                } else {
+                                    $cont++;
+                                }
+                            }
 
-            <!-- 3ボタン　-->
-            <div class="hostView_Btn">
-                <button id="eventControl_Btn">イベント一覧</button>
-                <button id="joinedPpl_Btn">応募者一覧</button>
-                <a href="create_event.php" id="hostpostBtn">イベント制作</a>
-            </div>
+                            //ユーザが一人もいなかったとき
+                            if (!$joinedUser) {
+                                echo '<div class="imgRow">'; //始まりのタグ
+                                echo "\n";
+                                echo '</div>'; //終わりのタグ
+                                echo "\n";
+                            }
+                            //ループを抜けた後タグを閉じていないときに閉じる
+                            if ($cont != 0) {
+                                echo '</div>'; //終わりのタグ
+                                echo "\n";
+                            }
 
-            <div id="switchPage">
-
-                <!-- イベント一覧　-->
-                <div class="eventControl">
-
-                    <?php foreach ($eventDatas as $eventData) : ?>
-                        <div class="place-content">
-
-                            <a href="event_Content.php?eventId=<?= $eventData['EVENT_ID'] ?>" class="col-md-12 col-lg-10 mx-auto item-box">
-                                <div class="event-item">
-                                    <diV class="col-md-7 center-item">
-                                        <div class="eventControl_Img">
-                                            <img src="<?= $eventData['ICON'] ?>" alt="">
-                                        </div>
-
-                                        <div class="information">
-                                            <p id="<?php if ($eventData['STATUS'] == "募集中") {
-                                                        echo 'event01_Status_Active';
-                                                    } else {
-                                                        echo 'event01_Status_Finished';
-                                                    }; ?>">
-                                                <?= $eventData['STATUS'] ?>
-                                            </p>
-                                            <h3><?= $eventData['EVENT_NAME'] ?></h3>
-                                            <div class="EvCon_Date">
-                                                <h4>日時</h4>
-                                                <p id="#"><?php echo date("Y年m月d日 H:i", strtotime($eventData['SCHEDULE'])) ?></p>
-                                            </div>
-                                            <div class="EvCon_Place">
-                                                <h4>場所</h4>
-                                                <p id="#"><?= $eventData['ADDRESS'] ?></p>
-                                            </div>
-                                            <div class="EvCon_Theme">
-                                                <h4>テーマ</h4>
-                                                <p id="#"><?= $eventData['THEME'] ?></p>
-                                            </div>
-                                        </div>
-                                    </diV>
-                                </div>
-                            </a>
+                            ?>
 
                         </div>
-                    <?php endforeach; ?>
-
+                    </div>
                 </div>
+            </div>
 
-                <!-- 応募者一覧　-->
-                <div class="joinedPpl_Control">
+        </div>
 
-                    <?php foreach ($registers as $register) : ?>
+    </section>
 
-                        <div class="place-content">
+    <!-- イベント一覧＋ 応募者一覧　エリア　-->
+    <section id="selfInfo_BottomArea">
+
+        <!-- 3ボタン　-->
+        <div class="hostView_Btn">
+            <button id="eventControl_Btn">イベント一覧</button>
+            <button id="joinedPpl_Btn">応募者一覧</button>
+            <a href="create_event.php" id="hostpostBtn">イベント制作</a>
+        </div>
+
+        <div id="switchPage">
+
+            <!-- イベント一覧　-->
+            <div class="eventControl">
+
+                <?php foreach ($eventDatas as $eventData) : ?>
+                    <div class="place-content">
+
+                        <a href="event_Content.php?eventId=<?= $eventData['EVENT_ID'] ?>" class="col-md-12 col-lg-10 mx-auto item-box">
                             <div class="event-item">
                                 <diV class="col-md-7 center-item">
-                                    <div class="joinedPpl_Control_Img">
-                                        <a href="userpage_ViewOnly.php">
-                                            <img src="<?= $register['ICON']; ?>" alt="">
-                                        </a>
+                                    <div class="eventControl_Img">
+                                        <img src="<?= $eventData['ICON'] ?>" alt="">
                                     </div>
 
-                                    <div class="informationUser">
-                                        <h3><?= $register['EVENT_NAME']; ?></h3>
-                                        <p><?= $register['USER_NAME']; ?></p>
-                                        <p><a href="mailto: <?= $register['EMAIL']; ?>"><?= $register['EMAIL']; ?></a></p>
-                                        <?php foreach ($ansdata as $ans) : ?>
-                                            <p><?= $ans ?></p>
-                                        <?php endforeach; ?>
+                                    <div class="information">
+                                        <p id="<?php if ($eventData['STATUS'] == "募集中"){
+                                            echo 'event01_Status_Active';
+                                        }else{
+                                            echo 'event01_Status_Finished';
+                                        };?>">
+                                            <?= $eventData['STATUS'] ?>
+                                        </p>
+                                        <h3><?= $eventData['EVENT_NAME'] ?></h3>
+                                        <div class="EvCon_Date">
+                                            <h4>日時</h4>
+                                            <p id="#"><?php echo date("Y年m月d日 H:i", strtotime($eventData['SCHEDULE'])) ?></p>
+                                        </div>
+                                        <div class="EvCon_Place">
+                                            <h4>場所</h4>
+                                            <p id="#"><?= $eventData['ADDRESS'] ?></p>
+                                        </div>
+                                        <div class="EvCon_Theme">
+                                            <h4>テーマ</h4>
+                                            <p id="#"><?= $eventData['THEME'] ?></p>
+                                        </div>
                                     </div>
                                 </diV>
                             </div>
-<<<<<<< HEAD
-                            </a>
-=======
                         </a>
 
                     </div>
@@ -290,22 +257,23 @@ $photodatas = TakePostData($id); //投稿した写真を取得
 
                                 </div>
                             </diV>
->>>>>>> 6c5f884025e6c9027fbbc6678074a9d81cc0dbdb
                         </div>
-                    <?php endforeach; ?>
+                        </a>
+                    </div>
+                <?php endforeach; ?>
 
-                </div>
             </div>
+        </div>
 
-        </section>
+    </section>
 
 
-        <?php include('./Navbar/footer.php'); ?>
+    <?php include('./Navbar/footer.php'); ?>
 
-        <a></a> <!-- To Top Apple Button-->
+    <a></a> <!-- To Top Apple Button-->
 
-        <script src="./js/navbar.js"></script>
-        <script src="./js/hostpage_AfterLogin.js"></script>
-    </body>
+    <script src="./js/navbar.js"></script>
+    <script src="./js/hostpage_AfterLogin.js"></script>
+</body>
 
-    </html>
+</html>
