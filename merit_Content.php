5cc8a8dff976e865818077a2eeb12b260482c1f8
LAMJOLIE <jolielam317@gmail.com>
2023-07-13 16:45:29 +0900

    修正

--- merit_Content.php
+++ merit_Content.php
@@ -28,7 +28,7 @@
     <section id="Merit_Area">
 
         <!-- メリット一覧 -->
-        <div data-aos="fade-up" data-aos-duration="1000" id="society">
+        <div data-aos="fade-up" data-aos-duration="500"id="society">
             <a href="#society"><img src="./image/merit1.png"></a>
             <p>社会課題の原因を学ぶこともできます。なぜ困難な状況になっている人がいるのかを考える、
                 あるいは自然環境の問題となる要因に目を向けていく、といったこともあるでしょう。
@@ -36,13 +36,13 @@
             </p>
         </div>
 
-        <div data-aos="fade-up" data-aos-duration="1000" id="friends">
+        <div data-aos="fade-up" data-aos-duration="500" id="friends">
             <p>参加するボランティアの団体や目的を選ぶ際は、同じボランティア活動に参加している人たちについても考慮しましょう。
                 共通の関心事を持つことで、周囲の参加者とより親しい関係を築くことができるかもしれません。</p>
             <a href="#friends"><img src="./image/merit2.png"></a>
         </div>
 
-        <div data-aos="fade-up" data-aos-duration="1000" id="communicate">
+        <div data-aos="fade-up" data-aos-duration="500" id="communicate">
             <a href="#communicate"><img src="./image/merit3.png"></a>
             <p>ボランティア活動は初対面の人と話す機会につながるため、社交スキルを磨くのにも適しているでしょう。
                 長時間を他者と協力して過ごし、傾聴力や人間関係管理能力などの社交スキルを活かすことで、
@@ -50,7 +50,7 @@
             </p>
         </div>
 
-        <div data-aos="fade-up" data-aos-duration="1000" id="jobs">
+        <div data-aos="fade-up" data-aos-duration="500" id="jobs">
             <p>貴重なスキルや経験が得られるだけでなく、ボランティア活動に参加することで、
                 将来のメンター（指導者）に出会ったり、仕事上の人脈が広がることが期待できるでしょう。
                 参加している活動の分野でキャリアを積むことを選択した場合は、人脈とともに仕事の可能性も広がるかもしれません。
